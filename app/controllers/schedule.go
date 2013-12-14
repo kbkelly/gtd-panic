05af0de56d4f7826e7f9ae0ca380b7914078a2a9
Kristopher Kelly <kristopherbkelly@gmail.com>
2013-12-14 00:26:03 -0800

    Show all events in 15 minute intervals

--- app/controllers/schedule.go
+++ app/controllers/schedule.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"os"
 	"io"
+	"time"
     "encoding/csv"
 	"github.com/robfig/revel"
 	"github.com/kbkelly/gtd-panic/app/models"
@@ -46,6 +47,15 @@ func readOmnifocusCsv() []*models.Event {
 	    	event := &models.Event{fields[2], "2013-12-13 10:00", "2013-12-13 11:00", false}
 	    	events = append(events, event)	    	
 	    }
+	}
+	// starting from now, place each event at 15 minute intervals
+	current := time.Now()
+	interval := time.Duration(15) * time.Minute
+	layout := "2006-01-02 15:04:05" // "Mon Jan 2 15:04:05 -0700 MST 2006"
+	for _, e := range events {
+		e.Start = current.Format(layout)
+		e.End = current.Add(interval).Format(layout)
+		current = current.Add(interval)
 	}
 	return events
 }
