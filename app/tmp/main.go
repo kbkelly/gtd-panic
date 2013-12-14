6f24441329fe9d1c955ff4a192929c77a4cfd119
Kristopher Kelly <kristopherbkelly@gmail.com>
2013-12-13 21:44:21 -0800

    Initial

---
+++ app/tmp/main.go
@@ -0,0 +1,324 @@
+// GENERATED CODE - DO NOT EDIT
+package main
+
+import (
+	"flag"
+	"reflect"
+	"github.com/robfig/revel"
+	_ "github.com/kbkelly/gtd-panic/app"
+	controllers "github.com/kbkelly/gtd-panic/app/controllers"
+	_ "github.com/kbkelly/gtd-panic/app/jobs"
+	models "github.com/kbkelly/gtd-panic/app/models"
+	tests "github.com/kbkelly/gtd-panic/tests"
+	_ "github.com/mattn/go-sqlite3"
+	controllers2 "github.com/robfig/revel/modules/jobs/app/controllers"
+	_ "github.com/robfig/revel/modules/jobs/app/jobs"
+	controllers1 "github.com/robfig/revel/modules/static/app/controllers"
+	_ "github.com/robfig/revel/modules/testrunner/app"
+	controllers0 "github.com/robfig/revel/modules/testrunner/app/controllers"
+)
+
+var (
+	runMode    *string = flag.String("runMode", "", "Run mode.")
+	port       *int    = flag.Int("port", 0, "By default, read from app.conf")
+	importPath *string = flag.String("importPath", "", "Go Import Path for the app.")
+	srcPath    *string = flag.String("srcPath", "", "Path to the source root.")
+
+	// So compiler won't complain if the generated code doesn't reference reflect package...
+	_ = reflect.Invalid
+)
+
+func main() {
+	flag.Parse()
+	revel.Init(*runMode, *importPath, *srcPath)
+	revel.INFO.Println("Running revel server")
+	
+	revel.RegisterController((*controllers.GorpController)(nil),
+		[]*revel.MethodType{
+			&revel.MethodType{
+				Name: "Begin",
+				Args: []*revel.MethodArg{ 
+				},
+				RenderArgNames: map[int][]string{ 
+				},
+			},
+			&revel.MethodType{
+				Name: "Commit",
+				Args: []*revel.MethodArg{ 
+				},
+				RenderArgNames: map[int][]string{ 
+				},
+			},
+			&revel.MethodType{
+				Name: "Rollback",
+				Args: []*revel.MethodArg{ 
+				},
+				RenderArgNames: map[int][]string{ 
+				},
+			},
+			
+		})
+	
+	revel.RegisterController((*controllers0.TestRunner)(nil),
+		[]*revel.MethodType{
+			&revel.MethodType{
+				Name: "Index",
+				Args: []*revel.MethodArg{ 
+				},
+				RenderArgNames: map[int][]string{ 
+					46: []string{ 
+						"testSuites",
+					},
+				},
+			},
+			&revel.MethodType{
+				Name: "Run",
+				Args: []*revel.MethodArg{ 
+					&revel.MethodArg{Name: "suite", Type: reflect.TypeOf((*string)(nil)) },
+					&revel.MethodArg{Name: "test", Type: reflect.TypeOf((*string)(nil)) },
+				},
+				RenderArgNames: map[int][]string{ 
+					69: []string{ 
+						"error",
+					},
+				},
+			},
+			&revel.MethodType{
+				Name: "List",
+				Args: []*revel.MethodArg{ 
+				},
+				RenderArgNames: map[int][]string{ 
+				},
+			},
+			
+		})
+	
+	revel.RegisterController((*controllers1.Static)(nil),
+		[]*revel.MethodType{
+			&revel.MethodType{
+				Name: "Serve",
+				Args: []*revel.MethodArg{ 
+					&revel.MethodArg{Name: "prefix", Type: reflect.TypeOf((*string)(nil)) },
+					&revel.MethodArg{Name: "filepath", Type: reflect.TypeOf((*string)(nil)) },
+				},
+				RenderArgNames: map[int][]string{ 
+				},
+			},
+			&revel.MethodType{
+				Name: "ServeModule",
+				Args: []*revel.MethodArg{ 
+					&revel.MethodArg{Name: "moduleName", Type: reflect.TypeOf((*string)(nil)) },
+					&revel.MethodArg{Name: "prefix", Type: reflect.TypeOf((*string)(nil)) },
+					&revel.MethodArg{Name: "filepath", Type: reflect.TypeOf((*string)(nil)) },
+				},
+				RenderArgNames: map[int][]string{ 
+				},
+			},
+			
+		})
+	
+	revel.RegisterController((*controllers2.Jobs)(nil),
+		[]*revel.MethodType{
+			&revel.MethodType{
+				Name: "Status",
+				Args: []*revel.MethodArg{ 
+				},
+				RenderArgNames: map[int][]string{ 
+					19: []string{ 
+						"entries",
+					},
+				},
+			},
+			
+		})
+	
+	revel.RegisterController((*controllers.Application)(nil),
+		[]*revel.MethodType{
+			&revel.MethodType{
+				Name: "AddUser",
+				Args: []*revel.MethodArg{ 
+				},
+				RenderArgNames: map[int][]string{ 
+				},
+			},
+			&revel.MethodType{
+				Name: "Index",
+				Args: []*revel.MethodArg{ 
+				},
+				RenderArgNames: map[int][]string{ 
+					47: []string{ 
+					},
+				},
+			},
+			&revel.MethodType{
+				Name: "Register",
+				Args: []*revel.MethodArg{ 
+				},
+				RenderArgNames: map[int][]string{ 
+					51: []string{ 
+					},
+				},
+			},
+			&revel.MethodType{
+				Name: "SaveUser",
+				Args: []*revel.MethodArg{ 
+					&revel.MethodArg{Name: "user", Type: reflect.TypeOf((*models.User)(nil)) },
+					&revel.MethodArg{Name: "verifyPassword", Type: reflect.TypeOf((*string)(nil)) },
+				},
+				RenderArgNames: map[int][]string{ 
+				},
+			},
+			&revel.MethodType{
+				Name: "Login",
+				Args: []*revel.MethodArg{ 
+					&revel.MethodArg{Name: "username", Type: reflect.TypeOf((*string)(nil)) },
+					&revel.MethodArg{Name: "password", Type: reflect.TypeOf((*string)(nil)) },
+				},
+				RenderArgNames: map[int][]string{ 
+				},
+			},
+			&revel.MethodType{
+				Name: "Logout",
+				Args: []*revel.MethodArg{ 
+				},
+				RenderArgNames: map[int][]string{ 
+				},
+			},
+			
+		})
+	
+	revel.RegisterController((*controllers.Hotels)(nil),
+		[]*revel.MethodType{
+			&revel.MethodType{
+				Name: "Index",
+				Args: []*revel.MethodArg{ 
+				},
+				RenderArgNames: map[int][]string{ 
+					37: []string{ 
+						"bookings",
+					},
+				},
+			},
+			&revel.MethodType{
+				Name: "List",
+				Args: []*revel.MethodArg{ 
+					&revel.MethodArg{Name: "search", Type: reflect.TypeOf((*string)(nil)) },
+					&revel.MethodArg{Name: "size", Type: reflect.TypeOf((*int)(nil)) },
+					&revel.MethodArg{Name: "page", Type: reflect.TypeOf((*int)(nil)) },
+				},
+				RenderArgNames: map[int][]string{ 
+					58: []string{ 
+						"hotels",
+						"search",
+						"size",
+						"page",
+						"nextPage",
+					},
+				},
+			},
+			&revel.MethodType{
+				Name: "Show",
+				Args: []*revel.MethodArg{ 
+					&revel.MethodArg{Name: "id", Type: reflect.TypeOf((*int)(nil)) },
+				},
+				RenderArgNames: map[int][]string{ 
+					89: []string{ 
+						"title",
+						"hotel",
+					},
+				},
+			},
+			&revel.MethodType{
+				Name: "Settings",
+				Args: []*revel.MethodArg{ 
+				},
+				RenderArgNames: map[int][]string{ 
+					93: []string{ 
+					},
+				},
+			},
+			&revel.MethodType{
+				Name: "SaveSettings",
+				Args: []*revel.MethodArg{ 
+					&revel.MethodArg{Name: "password", Type: reflect.TypeOf((*string)(nil)) },
+					&revel.MethodArg{Name: "verifyPassword", Type: reflect.TypeOf((*string)(nil)) },
+				},
+				RenderArgNames: map[int][]string{ 
+				},
+			},
+			&revel.MethodType{
+				Name: "ConfirmBooking",
+				Args: []*revel.MethodArg{ 
+					&revel.MethodArg{Name: "id", Type: reflect.TypeOf((*int)(nil)) },
+					&revel.MethodArg{Name: "booking", Type: reflect.TypeOf((*models.Booking)(nil)) },
+				},
+				RenderArgNames: map[int][]string{ 
+					144: []string{ 
+						"title",
+						"hotel",
+						"booking",
+					},
+				},
+			},
+			&revel.MethodType{
+				Name: "CancelBooking",
+				Args: []*revel.MethodArg{ 
+					&revel.MethodArg{Name: "id", Type: reflect.TypeOf((*int)(nil)) },
+				},
+				RenderArgNames: map[int][]string{ 
+				},
+			},
+			&revel.MethodType{
+				Name: "Book",
+				Args: []*revel.MethodArg{ 
+					&revel.MethodArg{Name: "id", Type: reflect.TypeOf((*int)(nil)) },
+				},
+				RenderArgNames: map[int][]string{ 
+					163: []string{ 
+						"title",
+						"hotel",
+					},
+				},
+			},
+			
+		})
+	
+	revel.DefaultValidationKeys = map[string]map[int]string{ 
+		"github.com/kbkelly/gtd-panic/app/controllers.Application.SaveUser": { 
+			55: "verifyPassword",
+			56: "verifyPassword",
+		},
+		"github.com/kbkelly/gtd-panic/app/controllers.Hotels.SaveSettings": { 
+			98: "verifyPassword",
+			100: "verifyPassword",
+		},
+		"github.com/kbkelly/gtd-panic/app/models.(*Hotel).Validate": { 
+			19: "hotel.Name",
+			21: "hotel.Address",
+			26: "hotel.City",
+			32: "hotel.State",
+			38: "hotel.Zip",
+			44: "hotel.Country",
+		},
+		"github.com/kbkelly/gtd-panic/app/models.(*User).Validate": { 
+			28: "user.Username",
+			36: "user.Name",
+		},
+		"github.com/kbkelly/gtd-panic/app/models.Booking.Validate": { 
+			34: "booking.User",
+			35: "booking.Hotel",
+			36: "booking.CheckInDate",
+			37: "booking.CheckOutDate",
+			39: "booking.CardNumber",
+			46: "booking.NameOnCard",
+		},
+		"github.com/kbkelly/gtd-panic/app/models.ValidatePassword": { 
+			44: "password",
+		},
+	}
+	revel.TestSuites = []interface{}{ 
+		(*tests.ApplicationTest)(nil),
+	}
+
+	revel.Run(*port)
+}
